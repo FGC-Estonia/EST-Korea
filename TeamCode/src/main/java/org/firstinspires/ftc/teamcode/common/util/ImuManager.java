82a2f2be9ae56d9bfa55f6e9387ab07b6d8154d5
EstiOliver <92375952+EstiOliver@users.noreply.github.com>
2026-07-20 13:44:03 +0300

    Fixed ports

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/util/ImuManager.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/util/ImuManager.java
@@ -30,11 +30,11 @@ public class ImuManager {
                 RevHubOrientationOnRobot.UsbFacingDirection usbDirection;
 
                 if (xDrive) {
-                    logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.LEFT;
-                    usbDirection = RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD;
+                    logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.RIGHT;
+                    usbDirection = RevHubOrientationOnRobot.UsbFacingDirection.UP;
                 } else {
-                    logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.LEFT;
-                    usbDirection = RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD;
+                    logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.RIGHT;
+                    usbDirection = RevHubOrientationOnRobot.UsbFacingDirection.UP;
                 }
 
                 RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(logoDirection, usbDirection);
@@ -50,8 +50,8 @@ public class ImuManager {
         }else {
             imu = hardwareMap.get(IMU.class, "imu");
 
-            RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.LEFT;
-            RevHubOrientationOnRobot.UsbFacingDirection usbDirection = RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD;
+            RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.RIGHT;
+            RevHubOrientationOnRobot.UsbFacingDirection usbDirection = RevHubOrientationOnRobot.UsbFacingDirection.UP;
 
             RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(logoDirection, usbDirection);
 
