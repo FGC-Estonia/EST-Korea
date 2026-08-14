766cb67769b0543b9274eb0b8f181b071e4d97f5
kartomiir <kartomiir@gmail.com>
2026-08-14 13:37:29 +0300

    Last commit but fixed
    title

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/util/HardwareConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/util/HardwareConstants.java
@@ -6,20 +6,20 @@ public class HardwareConstants {
 
     // Movement motors
     public static final String LEFT_FRONT_MOTOR = "Motor_Port_3_CH";
-    public static final String RIGHT_FRONT_MOTOR = "Motor_Port_0_CH";
+    public static final String RIGHT_FRONT_MOTOR = "Motor_Port_1_EH";
     public static final String LEFT_BACK_MOTOR = "Motor_Port_2_CH";
-    public static final String RIGHT_BACK_MOTOR = "Motor_Port_2_EH";
+    public static final String RIGHT_BACK_MOTOR = "Motor_Port_1_CH";
     // Rope motors
     public static final String ROPE_MOTOR = "Motor_Port_3_EH";
     // Other gadgets' motors
-    public static final String WHEEL_MOTOR = "Motor_Port_1_CH";
+    public static final String WHEEL_MOTOR = "Motor_Port_2_EH";
+    public static final String BALL_COLLECTOR_MOTOR = "Motor_Port_0_CH";
+    public static final String FEEDER_MOTOR = "Motor_Port_0_EH";
     public static final String REACHL_SERVO_MOTOR = "Servo_Port_1_CH";
     public static final String REACHR_SERVO_MOTOR = "Servo_Port_0_CH";
     public static final String LOCK_SERVO_MOTOR = "Servo_Port_3_CH";
     public static final String WINK_SERVO_MOTOR = "Servo_Port_4_CH";
-    public static final String BUDDY_SERVO_MOTOR = "Servo_Port_4_CH";
-    public static final String BALL_COLLECTOR_MOTOR = "Motor_Port_1_EH";
-    public static final String FEEDER_MOTOR = "Motor_Port_0_EH";
+    public static final String BUDDY_SERVO_MOTOR = "Servo_Port_5_CH";
 
 
 
