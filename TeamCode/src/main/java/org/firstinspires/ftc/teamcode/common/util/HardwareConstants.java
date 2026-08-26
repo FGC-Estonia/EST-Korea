522a9cc38a7fb2b77a21fb1fbb069caa5a88bc93
Saarmets <ralf.saarmets@gmail.com>
2026-08-26 11:45:00 +0300

    Update HardwareConstants.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/util/HardwareConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/util/HardwareConstants.java
@@ -3,21 +3,22 @@ package org.firstinspires.ftc.teamcode.common.util;
 public class HardwareConstants {
 
     // Movement motors
-    public static final String LEFT_FRONT_MOTOR = "Motor_Port_1_CH";
-    public static final String RIGHT_FRONT_MOTOR = "Motor_Port_2_CH";
-    public static final String LEFT_BACK_MOTOR = "Motor_Port_0_EH";
-    public static final String RIGHT_BACK_MOTOR = "Motor_Port_3_CH";
+    public static final String LEFT_FRONT_MOTOR = "Motor_Port_0_EH";
+    public static final String RIGHT_FRONT_MOTOR = "Motor_Port_1_CH";
+    public static final String LEFT_BACK_MOTOR = "Motor_Port_1_EH";
+    public static final String RIGHT_BACK_MOTOR = "Motor_Port_2_EH";
     // Rope motors
-    public static final String ROPE_MOTOR = "Motor_Port_0_CH";
+    public static final String CLIMB_MOTOR = "Motor_Port_3_EH";
     // Other gadgets' motors
-    public static final String WHEEL_MOTOR = "Motor_Port_1_EH";
-    public static final String BALL_COLLECTOR_MOTOR = "Motor_Port_2_EH";
-    public static final String FEEDER_MOTOR = "Motor_Port_3_EH";
-    public static final String REACHL_SERVO_MOTOR = "Servo_Port_3_CH";
-    public static final String REACHR_SERVO_MOTOR = "Servo_Port_4_EH";
-    public static final String LOCK_SERVO_MOTOR = "Servo_Port_5_EH";
+    public static final String THROW_MOTOR = "Motor_Port_0_CH";
+    public static final String BALL_COLLECTOR_MOTOR = "Motor_Port_2_CH";
+    public static final String FEEDER_MOTOR = "Motor_Port_3_CH";
+    public static final String REACHL_SERVO_MOTOR = "Servo_Port_4_EH";
+    public static final String REACHR_SERVO_MOTOR = "Servo_Port_5_EH";
+    public static final String LOCK_SERVO_MOTOR = "Servo_Port_0_CH";
     public static final String WINK_SERVO_MOTOR = "Servo_Port_4_CH";
     public static final String BUDDY_SERVO_MOTOR = "Servo_Port_5_CH";
-    public static final String WIGGLE_SERVO_MOTOR = "Servo_Port_0_CH";
+    public static final String WIGGLE_SERVO_MOTOR = "Servo_Port_0_EH";
+    public static final String WALL_SERVO_MOTOR = "Servo_Port_3_EH";
 
 }
